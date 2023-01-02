45be636c9c5a1fadecda386ad1779dde81f6c977
Petra Majsec <petra@spaceinch.com>
2023-01-02 22:30:50 +0100

    hamburger menu wooo

--- src/side-menu.js
+++ src/side-menu.js
@@ -2,12 +2,20 @@
 function myFunction() {
     var menuButton = document.getElementById("sidebar-menu");
     var overlay = document.getElementById("overlay");
+    var hamburgerMenu = document.getElementById("hamburger-menu");
     if (menuButton.style.display === "block") {
       menuButton.style.display = "none";
       overlay.style.display = "none";
+      hamburgerMenu.style.display = "block"; 
+      
+      hamburgerMenu.classList.toggle("change");
     } else {
       menuButton.style.display = "block";
       overlay.style.display = "block";
+      
+      hamburgerMenu.classList.toggle("change");
+      hamburgerMenu.style.display = "block"; 
 
+      
     }
   }
