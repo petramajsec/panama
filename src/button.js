3c36f4b8ac10614d599b0f60bfbbb62eec09c984
Petra Majsec <petra@spaceinch.com>
2023-01-23 12:42:10 +0100

    button successfull fix

--- src/button.js
+++ src/button.js
@@ -1,8 +1,8 @@
 $(".cta").on("click", function (e) {
-    console.log(e);
-    console.log(e.target);
-    $(e.target).addClass("active").delay(300).queue(function (next) {
-      $(e.target).removeClass("active");
-      next();
-    });
+  console.log(e);
+  console.log(e.target);
+  $(this).addClass("active").delay(300).queue(function (next) {
+    $(this).removeClass("active");
+    next();
   });
+});
